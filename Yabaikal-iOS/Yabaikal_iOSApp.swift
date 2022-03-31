d1564412c67708ee03fa1cad0fc2f1a47887542c
Roman Predein <roman@predein.com>
2022-03-31 12:16:09 +0800

    Initial Commit

---
+++ Yabaikal-iOS/Yabaikal_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yabaikal_iOSApp.swift
+//  Yabaikal-iOS
+//
+//  Created by Роман Предеин on 31.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Yabaikal_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
